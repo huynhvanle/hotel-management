cf48f6387ab55cbd6f73f8cb00a3a4d2c3af77c2
huynhvanle <vanhuynh6b@gmail.com>
2026-03-29 09:09:16 +0700

    init project

---
+++ src/test/java/com/web/hotel_management/HotelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.hotel_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
